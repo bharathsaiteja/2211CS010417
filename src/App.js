4f0d403150ca07c55ce89bff2640e528a0524f54
bharathsaiteja <129739145+bharathsaiteja@users.noreply.github.com>
2024-02-12 11:07:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import StudentList from './components/StudentList';
+
+const App = () => {
+  return (
+    <div className="App">
+<div className="container mt-3" ></div>
+      <h1>My App</h1>
+      <StudentList />
+    </div>
+  );
+};
+
+export default App;
